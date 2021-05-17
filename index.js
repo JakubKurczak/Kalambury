04c21ca779dfff65c9f2d8341376efca305e4713
JakubKurczak <jakubkurczak97@gmail.com>
2021-05-17 12:54:27 +0200

    init kalambury

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+var active_users = {};
+var active_users_names = [];
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/pages/index.html');
+});
+
+io.on('connection', (socket)=>{
+    console.log('a user connected');
+
+    var name = "Player "+socket.id;
+    active_users_names.push(name);
+    active_users[name] = socket;
+    socket.data.user_name = name;
+
+    io.emit('users', active_users_names);
+
+    socket.on('chat message', (msg)=>{
+        console.log('message: ' + msg);
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+
+    socket.on('disconnect', ()=>{
+        console.log('a user disconnected');
+        var index = 0;
+        for( const i in active_users_names){
+            if(socket.data.user_name == active_users_names[i]){
+
+                active_users_names.splice(index,1);
+                io.emit('users', active_users_names);
+                break;
+            }
+            index += 1;
+
+        };
+
+    });
+
+    socket.on('mouse', (data)=>{
+        io.emit('mouse',data);        
+    });
+
+});
+
+server.listen(3000, ()=>{
+    console.log('listening on *:3000');
+});
---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup(){
+    var canvas = createCanvas(400, 400);
+    canvas.parent('canvas-box');
+}
+
+function draw(){
+    background(127);
+}
